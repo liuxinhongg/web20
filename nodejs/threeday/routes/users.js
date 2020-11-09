80270672274437bc74c46ca3e46c831b2a40cb08
liuxinhongg <644391351@qq.com>
2020-11-09 14:53:06 +0800

    wechat

--- nodejs/threeday/routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var db = require("../db.js");
-var cbfile = require("../cbfile/cb.js")
-
-router.post("/register",cbfile.UserRegister)
-router.post("/login", cbfile.userLogin)
-module.exports = router;
